74ad89e6292eb541619d6500979d84cc64a20bed
Antonio Cozzi <antoniocozzi2@gmail.com>
2017-02-11 01:39:48 +0100

    Add buttons for call the user, locate the user and send a mail to the user

--- app/src/main/java/com/example/anton/primoesempio/MainActivity.java
+++ app/src/main/java/com/example/anton/primoesempio/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.anton.primoesempio;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,37 +13,62 @@ import android.widget.TextView;
  * Created by anton on 08/02/2017.
  */
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    int size = 0;
+    Button change, go, call, send;
+    TextView name, position, date, tel_num;
 
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         setTitle(R.string.app_name);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView title = (TextView) findViewById(R.id.title);
-        final TextView subtitle = (TextView) findViewById(R.id.subtitle);
-        final TextView date = (TextView) findViewById(R.id.date);
-        final TextView loremIpsum = (TextView) findViewById(R.id.loremIpsum);
-        final Button read = (Button) findViewById(R.id.read);
-        final Button change = (Button) findViewById(R.id.change);
+        change = (Button) findViewById(R.id.change);
+        change.setOnClickListener(this);
 
-        read.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                size += 1;
-                loremIpsum.setTextSize(size);
-            }
-        });
+        go = (Button) findViewById(R.id.go);
+        go.setOnClickListener(this);
 
-        change.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RelativeActivity.class);
-                startActivity(intent);
-            }
-        });
+        call = (Button) findViewById(R.id.call);
+        call.setOnClickListener(this);
+
+        send = (Button) findViewById(R.id.email);
+        send.setOnClickListener(this);
+
+        name = (TextView) findViewById(R.id.name);
+        position = (TextView) findViewById(R.id.position);
+        date = (TextView) findViewById(R.id.date);
+        tel_num = (TextView) findViewById(R.id.phone_number);
+    }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.change:
+                    Intent intent = new Intent(MainActivity.this, RelativeActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.call :
+                    Intent intent2 = new Intent();
+                    intent2.setAction(Intent.ACTION_VIEW);
+                    Uri uri = Uri.parse("tel:"+tel_num.getText().toString());
+                    intent2.setData(uri);
+                    startActivity(intent2);
+                    break;
+                case R.id.go:
+                    Intent intent3 = new Intent();
+                    intent3.setAction(Intent.ACTION_VIEW);
+                    Uri uri2 = Uri.parse("geo: 0,0?q=m"+position.getText().toString());
+                    intent3.setData(uri2);
+                    startActivity(intent3);
+                    break;
+                case R.id.email:
+                    Intent intent4 = new Intent();
+                    intent4.setAction(Intent.ACTION_VIEW);
+                    Uri uri3 = Uri.parse("mailto:"+send.getText().toString());
+                    intent4.setData(uri3);
+                    startActivity(intent4);
+        }
 
     }
 }
--- app/src/main/java/com/example/anton/primoesempio/RelativeActivity.java
+++ app/src/main/java/com/example/anton/primoesempio/RelativeActivity.java
@@ -5,32 +5,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import static android.view.View.*;
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
 /**
  * Created by anton on 08/02/2017.
  */
 
 public class RelativeActivity extends AppCompatActivity {
+
+    boolean giaMandato = false;
+
     protected void onCreate(Bundle savedInstanceState) {
         setTitle(R.string.app_name);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relative);
 
-        final ImageView image = (ImageView)findViewById(R.id.image);
+        final ImageView image = (ImageView) findViewById(R.id.image);
         image.setVisibility(View.INVISIBLE);
-        final Button fattiMandare = (Button)findViewById(R.id.fattiMandare);
+        final Button fattiMandare = (Button) findViewById(R.id.fattiMandare);
 
-        fattiMandare.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(image.getVisibility() == View.INVISIBLE) {
+    fattiMandare.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (giaMandato = true) {
+                Toast.makeText(RelativeActivity.this, getString(R.string.gia_mandato), Toast.LENGTH_SHORT).show();
+                finish();
+                if (image.getVisibility() == View.INVISIBLE) {
                     image.setVisibility(VISIBLE);
-                }
-                else
+                } else {
                     image.setVisibility(INVISIBLE);
+                }
+            }
+        }
+    });
+        fattiMandare.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(RelativeActivity.this, getString(R.string.smettila), Toast.LENGTH_SHORT).show();
+                giaMandato = true;
+                finish();
+                return false;
             }
         });
+
+
     }
 }
